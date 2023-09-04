6182d07c91213723f98e0cee609c6adea1263ac9
unknown <vasanthgamer2000@gmail.com>
2023-09-04 15:24:15 +0530

    Changes have been committed

---
+++ Assisted_Practice_Phase3/Phase3Section2.18/Phase3Section2.18/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phase3Section2._18.Models;
+using System.Diagnostics;
+
+namespace Phase3Section2._18.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult StudentEditView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ContentResult StudentEditView(StudentModel model)
+        {
+            String result = model.Name + "," + model.Address + "," + model.WhichClass + "," + model.TotalMarks.ToString();
+            return Content("Form was submitted:\n" + result);
+        }
+
+    }
+}
---
+++ Assisted_Practice_Phase3/Phase3Section2.18/Phase3Section2.18/Models/StudentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Phase3Section2._18.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string WhichClass { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+
+    }
+}
