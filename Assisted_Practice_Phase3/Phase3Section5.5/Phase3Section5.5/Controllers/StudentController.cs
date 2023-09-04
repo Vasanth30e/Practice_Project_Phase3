e8a533089f6289d87318d8253159fe1862b1ccf4
unknown <vasanthgamer2000@gmail.com>
2023-09-04 16:40:25 +0530

    Changes have been committed

---
+++ Assisted_Practice_Phase3/Phase3Section5.5/Phase3Section5.5/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+
+namespace Phase3Section5._5.Controllers
+{
+    public class StudentController : ApiController
+    {
+        Models.Student[] students = new Models.Student[]{
+         new  Models.Student {Name="Walter", Class="7A", Address="Address 1", Email="walter@email.com" },
+         new Models.Student {Name="Matthew", Class="7A", Address="Address 2", Email="matthew@email.com" },
+         new Models.Student {Name="Anne", Class="7A", Address="Address 3", Email="anne@email.com" },
+        };
+
+        public IEnumerable<Models.Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        public IHttpActionResult GetStudent(string name)
+        {
+            var student = students.FirstOrDefault((p) => p.Name == name);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+    }
+}
---
+++ Assisted_Practice_Phase3/Phase3Section5.5/Phase3Section5.5/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Phase3Section5._5.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+    }
+}
