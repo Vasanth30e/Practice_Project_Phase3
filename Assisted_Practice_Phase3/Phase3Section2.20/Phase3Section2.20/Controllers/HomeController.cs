c285fc752c284e4694c90d388062fe43b957c8d3
unknown <vasanthgamer2000@gmail.com>
2023-09-04 15:29:48 +0530

    Changes have been committed

---
+++ Assisted_Practice_Phase3/Phase3Section2.20/Phase3Section2.20/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phase3Section2._20.Models;
+using System.Diagnostics;
+using System.Text;
+
+namespace Phase3Section2._20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(StudentModel model)
+        {
+            StringBuilder sb = new StringBuilder("Form data:\n");
+            sb.Append(model.Name + ", " + model.Address + "," + model.Class + "," + model.Email);
+            return Content(sb.ToString());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assisted_Practice_Phase3/Phase3Section2.20/Phase3Section2.20/Models/StudentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Phase3Section2._20.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public string Email { get; set; }
+
+    }
+}
